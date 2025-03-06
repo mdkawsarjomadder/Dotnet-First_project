8966dd9559cf6aaa6cd2ea38f75f4985a62a0c4c
Md Kawar <60819454+mdkawsarjomadder@users.noreply.github.com>
2025-03-07 02:46:27 +0600

    code

--- Program.cs
+++ Program.cs
@@ -1,8 +1,37 @@
 var builder = WebApplication.CreateBuilder(args);
 
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
 var app = builder.Build();
-  app.UseHttpsRedirection();
- Console.WriteLine();
+
+if(app.Environment.IsDevelopment()){
+  app.UseSwagger();
+  app.UseSwaggerUI();
+}
+app.UseHttpsRedirection();
+
+app.MapGet("/", () =>{
+return "API Method";
+});
+
+app.MapGet("/hello", () =>{
+return "Get Mathode : Hello! MapGet";
+});
  
+app.MapPost("/hello", () =>{
+return "Post Mathode : Hello! MapPost";
+});
+
+app.MapPut("/hello", () =>{
+return "put Mathode : Hello! MapPut";
+});
+
+app.MapDelete("/hello", () =>{
+return "Delete Mathode : Hello! MapDelete";
+});
+
+
+
 app.Run();
 
