d18f01dcbf836ee07c0faa9b4f8629805ab4c36f
Md Kawar <60819454+mdkawsarjomadder@users.noreply.github.com>
2025-03-08 12:45:25 +0600

    DTO

--- Program.cs
+++ Program.cs
@@ -1,3 +1,5 @@
+using System.Net.Security;
+
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddEndpointsApiExplorer();
@@ -11,27 +13,63 @@ if(app.Environment.IsDevelopment()){
 }
 app.UseHttpsRedirection();
 
-app.MapGet("/", () =>{
-return "API Method";
-});
+app.MapGet("/", () =>"API single Method");
 
-app.MapGet("/hello", () =>{
-return "Get Mathode : Hello! MapGet";
-});
- 
-app.MapPost("/hello", () =>{
-return "Post Mathode : Hello! MapPost";
-});
-
-app.MapPut("/hello", () =>{
-return "put Mathode : Hello! MapPut";
-});
-
-app.MapDelete("/hello", () =>{
-return "Delete Mathode : Hello! MapDelete";
-});
-
-
+app.MapGet("/products",() =>
+     {
+      var products = new List<productsDto>(){
+           new productsDto ("redmi note 12",12000),
+           new productsDto ("redmi note 14",15000),
+      };
+   return  Results.Ok(products) ; //200
+     });
 
 app.Run();
+
+public record productsDto(string name, decimal price);
+
+
+
+
+
+
+
+// app.MapGet("/hello",() =>
+//     {
+  // var response =  new { Message = "MapGat single Method", success= true};
+                       //html create
+//    return  Results.Content("<h1>Hello World!</h1>", "text/html") ; //200
+//      });
+
+//      app.MapPost("/hello", () =>{
+//           return  Results.Created();   // new create......... 201
+//      });
+
+//      app.MapPut("/hello", () =>{
+//         return Results.NoContent();//204 
+//      });
+
+//      app.MapDelete("/hello", () =>{
+//            return Results.NoContent(); //204
+//      });
+
+// app.MapPut("/hello",() =>
+//     {
+//   var response =  new { Message = "MapPut single Method", success= true};
+//    return response;
+//      });
+
+// app.MapPost("/hello",() =>
+//     {
+//   var response =  new { Message = "Map Post single Method", success= true};
+//    return response;
+//      });
+ 
+
+
+//   app.MapDelete("/hello",() =>{
+//     var response = new { message = "Map Delete json Method",success = true};
+//     return response;
+//   });
+
 
